75088528c281904d1cc1fb442248de3f053cc67f
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2023-02-04 19:29:26 +0000

    Returns true given multiple correct bracket pairs

--- js/6_kyu/validBraces.js
+++ js/6_kyu/validBraces.js
@@ -1,6 +1,26 @@
 function validBraces(braces) {
   let validBraces = ['()', '[]', '{}'];
-  return (validBraces.includes(braces)) ? true: false;
+  let unmatchedBraceCount = 0;
+
+  let bracePairs = [];
+  let bracesArray = braces.split("");
+
+  // create array of brace pairs until not enough left to make a pair
+  while(bracesArray.length > 1){
+    let openingBrace = bracesArray.shift();
+    let closingBrace = bracesArray.pop();
+    let bracePair = openingBrace + closingBrace;
+    bracePairs.push(bracePair);
+  }
+
+  // check if any brace pair is invalid
+  bracePairs.forEach(bracePair => {
+    if(!validBraces.includes(bracePair)){
+      unmatchedBraceCount ++;
+    }
+  });
+
+  return (unmatchedBraceCount > 0)? false : true;
 }
 
 module.exports = validBraces;
--- js/__tests__/6_kyu/validBraces.spec.js
+++ js/__tests__/6_kyu/validBraces.spec.js
@@ -6,4 +6,16 @@ describe('validBraces function', () => {
          expect(validBraces('[]')).toEqual(true);
          expect(validBraces('{}')).toEqual(true);
     });
+
+    test('returns false 2 different brackets', () => {
+        expect(validBraces('[}')).toEqual(false);
+        expect(validBraces('{)')).toEqual(false);
+        expect(validBraces('(]')).toEqual(false);
+    });
+
+    test('returns true given multiple correct brackets', () => {
+        expect(validBraces('({})')).toEqual(true);
+        expect(validBraces('(())')).toEqual(true);
+        expect(validBraces('{[()]}')).toEqual(true);
+    });
 });
