478074dcb323584d2083f60f35ffdf841a64ea5c
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2024-01-08 17:34:55 +0800

    validBraces returns an empty string given one"

---
+++ js/6_kyu/splitStrings.js
@@ -0,0 +1,5 @@
+function validBraces() {
+    return '';
+}
+
+module.exports = validBraces;
---
+++ js/__tests__/6_kyu/splitStrings.spec.js
@@ -0,0 +1,7 @@
+const splitStrings = require('../../6_kyu/splitStrings');
+
+describe('splitStrings function', () => {
+    test('returns an empty string given an empty string', () => {
+         expect(splitStrings('')).toEqual('');
+    });
+});
