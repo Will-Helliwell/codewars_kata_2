0b3e2dea83cfc6c2437b3b5b9c0943c547c5c9bd
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2024-01-08 21:38:56 +0800

    validBraces returns correctly given an even number of chars"

--- js/6_kyu/splitStrings.js
+++ js/6_kyu/splitStrings.js
@@ -1,5 +1,14 @@
-function validBraces() {
-    return '';
+function validBraces(str) {
+  const returnArr = [];
+  const strLength = str.length;
+  for (let i = 0; i < strLength; i++) {
+    // if on an even char, add it and the one before to the return array
+    if ((i+1) % 2 == 0) {
+      let newSubString = str[i - 1] + str[i];
+      returnArr.push(newSubString);
+    }
+  }
+  return returnArr;
 }
 
 module.exports = validBraces;
--- js/__tests__/6_kyu/splitStrings.spec.js
+++ js/__tests__/6_kyu/splitStrings.spec.js
@@ -1,7 +1,13 @@
 const splitStrings = require('../../6_kyu/splitStrings');
 
 describe('splitStrings function', () => {
-    test('returns an empty string given an empty string', () => {
-         expect(splitStrings('')).toEqual('');
+    test('returns an empty array given an empty string', () => {
+         expect(splitStrings('')).toEqual([]);
+    });
+
+    test('returns correctly given an even number of chars', () => {
+         expect(splitStrings('ab')).toEqual(['ab']);
+         expect(splitStrings('abcd')).toEqual(['ab', 'cd']);
+         expect(splitStrings('abcdef')).toEqual(['ab', 'cd', 'ef']);
     });
 });
