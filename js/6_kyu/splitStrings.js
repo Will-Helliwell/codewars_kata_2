4156300607f625796a2a98bd23925de26f2fad07
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2024-01-09 13:05:30 +0800

    Add comment

--- js/6_kyu/splitStrings.js
+++ js/6_kyu/splitStrings.js
@@ -3,10 +3,11 @@ function validBraces(str) {
   const strLength = str.length;
   for (let i = 0; i < strLength; i++) {
     let newSubString = "";
-    // if on an even char, add it and the one before to the return array
+    // if on an even char, add it and the preceding to the return array
     if ((i + 1) % 2 == 0) {
       newSubString = str[i - 1] + str[i];
       returnArr.push(newSubString);
+      // if on the last char and it is odd, follow it by an underscore and add to the return array
     } else if (i == strLength - 1) {
       newSubString = str[i] + "_";
       returnArr.push(newSubString);
