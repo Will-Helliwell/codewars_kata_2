6c022e12f32a1e0e3656d54890ae8bda3fbcccfc
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2024-02-02 15:38:45 +0800

    pickPeaks guards against incorrect argument types.

--- js/5_kyu/pick_peaks.js
+++ js/5_kyu/pick_peaks.js
@@ -1,5 +1,12 @@
-function pickPeaks(arr){
-    return {pos:[],peaks:[]};
+function pickPeaks(arr) {
+  // guard against incorrect argument types
+  console.log(typeof arr);
+  console.log(arr);
+  if (!Array.isArray(arr)) {
+    throw new Error("argument must be of type array");
+  }
+
+  return { pos: [], peaks: [] };
 }
 
 module.exports = pickPeaks;
--- js/__tests__/5_kyu/pick_peaks.spec.js
+++ js/__tests__/5_kyu/pick_peaks.spec.js
@@ -1,8 +1,14 @@
-const pickPeaks = require('../../5_kyu/pick_peaks');
+const pickPeaks = require("../../5_kyu/pick_peaks");
 
-describe('pickPeaks function', () => {
-    test('returns correctly given no peaks', () => {
-         expect(pickPeaks([])).toEqual({pos:[],peaks:[]});
-         expect(pickPeaks([0,0,0])).toEqual({pos:[],peaks:[]});
-    });
+describe("pickPeaks function", () => {
+  test("returns an error if the wrong type of argument is passed", () => {
+    expect(() => {
+      pickPeaks("");
+    }).toThrow("argument must be of type array");
+  });
+
+  test("returns correctly given no peaks", () => {
+    expect(pickPeaks([])).toEqual({ pos: [], peaks: [] });
+    expect(pickPeaks([0, 0, 0])).toEqual({ pos: [], peaks: [] });
+  });
 });
