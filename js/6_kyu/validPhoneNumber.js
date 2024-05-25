9d0cd93dfaeb1185c1a73a460916556e04ba67b3
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2024-05-25 18:43:39 +0800

    First section and single whitespace validated correctly.

--- js/6_kyu/validPhoneNumber.js
+++ js/6_kyu/validPhoneNumber.js
@@ -2,6 +2,35 @@ function validPhoneNumber(phoneNumber){
     if (typeof(phoneNumber) !== 'string') {
         return false;
     }
+
+    const splitOnSpaces = phoneNumber.split(" ");
+
+    // valdiate first section
+    const firstSection = splitOnSpaces[0];
+    if(firstSection.length !== 5){
+        return false;
+    }
+
+    const firstSectionFirstChar = firstSection[0];
+    const firstSectionLastChar = firstSection[firstSection.length - 1];
+    if(firstSectionFirstChar !== '(' || firstSectionLastChar !== ')') {
+        return false;
+    } 
+
+    const firstSectionMiddleThreeChar = firstSection.slice(1,4);
+    if (!isNumeric(firstSectionMiddleThreeChar)) {
+        return false;
+    }
+
+    // validate correctly spaced single whitespace
+     if (phoneNumber[5] !== ' ') return false;
+
+    return true;
+
+
+    function isNumeric(value) {
+        return /^\d+$/.test(value);
+    }
 }
 
 module.exports = validPhoneNumber;
--- js/__tests__/6_kyu/validPhoneNumber.spec.js
+++ js/__tests__/6_kyu/validPhoneNumber.spec.js
@@ -1,14 +1,25 @@
-const findUniq = require('../../6_kyu/validPhoneNumber');
+const validPhoneNumber = require("../../6_kyu/validPhoneNumber");
 
-describe('validPhoneNumber function', () => {
-    test('it returns false given any non-string', () => {
-         expect(findUniq(123)).toEqual(false);
-         expect(findUniq(true)).toEqual(false);
-         expect(findUniq(12.2)).toEqual(false);
-         expect(findUniq([])).toEqual(false);
-         expect(findUniq({})).toEqual(false);
-         expect(findUniq(undefined)).toEqual(false);
-         expect(findUniq(NaN)).toEqual(false);
-         expect(findUniq(null)).toEqual(false);
-    });
+describe("validPhoneNumber function", () => {
+  test("it returns false given any non-string", () => {
+    expect(validPhoneNumber(123)).toEqual(false);
+    expect(validPhoneNumber(true)).toEqual(false);
+    expect(validPhoneNumber(12.2)).toEqual(false);
+    expect(validPhoneNumber([])).toEqual(false);
+    expect(validPhoneNumber({})).toEqual(false);
+    expect(validPhoneNumber(undefined)).toEqual(false);
+    expect(validPhoneNumber(NaN)).toEqual(false);
+    expect(validPhoneNumber(null)).toEqual(false);
+  });
+
+  test("it returns true only for the correct string format", () => {
+    // first section
+    expect(validPhoneNumber("(123) 456-7890")).toEqual(true);
+    expect(validPhoneNumber("(123( 456-7890")).toEqual(false);
+    expect(validPhoneNumber("(1234) 456-7890")).toEqual(false);
+    expect(validPhoneNumber("(12x) 456-7890")).toEqual(false);
+
+    // single whitespace
+    expect(validPhoneNumber("(123)456-7890")).toEqual(false);
+  });
 });
