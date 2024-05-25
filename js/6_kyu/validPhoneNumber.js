9e1d2fbcf3d4aaee9986a9502a515ad586d1ee2b
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2024-05-25 20:13:50 +0800

    Add comments and change function name.

--- js/6_kyu/validPhoneNumber.js
+++ js/6_kyu/validPhoneNumber.js
@@ -18,43 +18,47 @@ function validPhoneNumber(phoneNumber) {
 }
 
 function isFirstSectionValid(firstSection) {
-    // length
-    if (firstSection.length !== 5) return false;
+  // length
+  if (firstSection.length !== 5) return false;
 
-    // bracket position
-    const firstSectionFirstChar = firstSection[0];
-    const firstSectionLastChar = firstSection[firstSection.length - 1];
-    if (firstSectionFirstChar !== "(" || firstSectionLastChar !== ")")
-      return false;
+  // bracket position
+  const firstSectionFirstChar = firstSection[0];
+  const firstSectionLastChar = firstSection[firstSection.length - 1];
+  if (firstSectionFirstChar !== "(" || firstSectionLastChar !== ")")
+    return false;
 
-    // middle number
-    const firstSectionMiddleThreeChar = firstSection.slice(1, 4);
-    if(!isValidNumber(firstSectionMiddleThreeChar, 3)) return false;
+  // middle number
+  const firstSectionMiddleThreeChar = firstSection.slice(1, 4);
+  if (!isValidLengthNumber(firstSectionMiddleThreeChar, 3)) return false;
 
-    return true;
-  }
+  return true;
+}
 
-  function isSecondSectionValid(secondSection) {
-    if (secondSection.length !== 8) return false;
-    if (secondSection[3] !== "-") return false;
+function isSecondSectionValid(secondSection) {
+// length and hyphen position
+  if (secondSection.length !== 8) return false;
+  if (secondSection[3] !== "-") return false;
 
-    const secondSectionSplitByHyphen = secondSection.split("-");
-    const secondSectionFirstNumber = secondSectionSplitByHyphen[0];
-    if(!isValidNumber(secondSectionFirstNumber, 3)) return false;
+  // first number
+  const secondSectionSplitByHyphen = secondSection.split("-");
+  const secondSectionFirstNumber = secondSectionSplitByHyphen[0];
+  if (!isValidLengthNumber(secondSectionFirstNumber, 3)) return false;
 
-    const secondSectionSecondNumber = secondSectionSplitByHyphen[1];
-    if (!isValidNumber(secondSectionSecondNumber, 4)) return false;
+  // second number
+  const secondSectionSecondNumber = secondSectionSplitByHyphen[1];
+  if (!isValidLengthNumber(secondSectionSecondNumber, 4)) return false;
 
-    return true;
-  }
+  return true;
+}
 
-  function isValidNumber(number, numberLength) {
-    if (number.length !== numberLength || !isNumeric(number)) return false;
+function isValidLengthNumber(number, numberLength) {
+  if (number.length !== numberLength || !isNumeric(number)) return false;
 
-    return true;
-  }
-  function isNumeric(value) {
-    return /^\d+$/.test(value);
-  }
+  return true;
+}
+
+function isNumeric(value) {
+  return /^\d+$/.test(value);
+}
 
 module.exports = validPhoneNumber;
