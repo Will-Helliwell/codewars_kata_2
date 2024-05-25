9a8a69a3b396c27ce93a2a993d4542163c953e7e
Will-Helliwell <68912961+Will-Helliwell@users.noreply.github.com>
2024-05-25 19:12:09 +0800

    Abstract first section validation function.

--- js/6_kyu/validPhoneNumber.js
+++ js/6_kyu/validPhoneNumber.js
@@ -1,44 +1,62 @@
-function validPhoneNumber(phoneNumber){
-    if (typeof(phoneNumber) !== 'string') {
-        return false;
-    }
+function validPhoneNumber(phoneNumber) {
+  if (typeof phoneNumber !== "string") {
+    return false;
+  }
 
-    const splitOnSpaces = phoneNumber.split(" ");
+  const splitOnSpaces = phoneNumber.split(" ");
 
-    // valdiate first section
-    const firstSection = splitOnSpaces[0];
-   
-    if(firstSection.length !== 5) return false;
+  // valdiate first section
+  const firstSection = splitOnSpaces[0];
 
+  if (!isFirstSectionValid(firstSection)) return false;
+
+  // validate correctly spaced single whitespace
+  if (phoneNumber[5] !== " ") return false;
+
+  // validate second section
+  const secondSection = splitOnSpaces[1];
+
+  if (secondSection.length !== 8) return false;
+
+  if (secondSection[3] !== "-") return false;
+
+  const secondSectionSplitByHyphen = secondSection.split("-");
+  const secondSectionFirstNumber = secondSectionSplitByHyphen[0];
+  if (
+    secondSectionFirstNumber.length !== 3 ||
+    !isNumeric(secondSectionFirstNumber)
+  )
+    return false;
+
+  const secondSectionSecondNumber = secondSectionSplitByHyphen[1];
+  if (
+    secondSectionSecondNumber.length !== 4 ||
+    !isNumeric(secondSectionSecondNumber)
+  )
+    return false;
+
+  return true;
+
+  function isFirstSectionValid(firstSection) {
+    // length
+    if (firstSection.length !== 5) return false;
+
+    // bracket position
     const firstSectionFirstChar = firstSection[0];
     const firstSectionLastChar = firstSection[firstSection.length - 1];
-    if(firstSectionFirstChar !== '(' || firstSectionLastChar !== ')')  return false;
+    if (firstSectionFirstChar !== "(" || firstSectionLastChar !== ")")
+      return false;
 
-    const firstSectionMiddleThreeChar = firstSection.slice(1,4);
-    if (!isNumeric(firstSectionMiddleThreeChar)) return false;
-
-    // validate correctly spaced single whitespace
-     if (phoneNumber[5] !== ' ') return false;
-
-    // validate second section
-    const secondSection = splitOnSpaces[1];
-
-    if(secondSection.length !== 8) return false;
-    
-    if(secondSection[3] !== '-') return false;
-
-    const secondSectionSplitByHyphen = secondSection.split("-");
-    const secondSectionFirstNumber = secondSectionSplitByHyphen[0];
-    if(secondSectionFirstNumber.length !== 3 || !isNumeric(secondSectionFirstNumber)) return false;
-
-    const secondSectionSecondNumber = secondSectionSplitByHyphen[1];
-    if(secondSectionSecondNumber.length !== 4 || !isNumeric(secondSectionSecondNumber)) return false;
+    // middle number
+    const firstSectionMiddleThreeChar = firstSection.slice(1, 4);
+    if (firstSectionMiddleThreeChar.length !== 3 || !isNumeric(firstSectionMiddleThreeChar)) return false;
 
     return true;
+  }
 
-    function isNumeric(value) {
-        return /^\d+$/.test(value);
-    }
+  function isNumeric(value) {
+    return /^\d+$/.test(value);
+  }
 }
 
 module.exports = validPhoneNumber;
