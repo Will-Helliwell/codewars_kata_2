5b109bc313ac3075a32764b8562b127b26588732
Will-Helliwell <wj.helliwell@gmail.com>
2024-10-25 08:10:20 +0800

    Passes all tests.

--- js/6_kyu/backspacesInString.js
+++ js/6_kyu/backspacesInString.js
@@ -1,5 +1,14 @@
-function cleanString() {
-    return 'test';
+function cleanString(s) {
+    let result = [];
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '#') {
+            result.pop();
+        } else {
+            result.push(s[i]);
+        }
+    }
+    return result.join('');
 }
 
 module.exports = cleanString;
--- js/__tests__/6_kyu/backspacesInString.spec.js
+++ js/__tests__/6_kyu/backspacesInString.spec.js
@@ -2,6 +2,13 @@ const cleanString = require('../../6_kyu/backspacesInString');
 
 describe('cleanString', () => {
     it('should return a string', () => {
-        expect(typeof cleanString()).toBe('string');
+        expect(typeof cleanString('')).toBe('string');
+    });
+
+    it('should return the correct string', () => {
+        expect(cleanString('abc#d##c')).toBe('ac');
+        expect(cleanString('abc##d######')).toBe('');
+        expect(cleanString('#######')).toBe('');
+        expect(cleanString('')).toBe('');
     });
 });
